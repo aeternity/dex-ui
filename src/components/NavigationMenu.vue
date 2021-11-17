e78b1625316d8de4dc70c783222b77b7f35f9706
Nikita Cedrik <nikita.cedrik@gmail.com>
2021-11-17 12:09:43 +1000

    feat: add NavigationMenu component

---
+++ src/components/NavigationMenu.vue
@@ -0,0 +1,36 @@
+<template>
+  <nav class="navigation-menu">
+    <router-link to="/">Swap</router-link>
+    <router-link to="/about">Pool</router-link>
+  </nav>
+</template>
+
+<style lang="scss" scoped>
+.navigation-menu {
+  display: flex;
+  border-radius: 16px;
+  background: rgb(25, 27, 31);
+  padding: 4px;
+  border: 1px solid rgb(44, 47, 54);
+
+  a {
+    color: rgb(195, 197, 203);
+    padding: 8px 12px;
+    text-decoration: none;
+
+    &:not(:last-of-type) {
+      margin-right: 10px;
+    }
+
+    &:hover {
+      color: white
+    }
+
+    &.router-link-exact-active {
+      border-radius: 12px;
+      color: white;
+      background-color: rgb(44, 47, 54);
+    }
+  }
+}
+</style>
