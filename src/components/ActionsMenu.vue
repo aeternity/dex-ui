c4ed619fa81295552e7108502960ac4678d19e55
Nikita Cedrik <nikita.cedrik@gmail.com>
2021-11-17 13:01:43 +1000

    feat: add ActionsMenu component

---
+++ src/components/ActionsMenu.vue
@@ -0,0 +1,49 @@
+<template>
+  <div
+    class="actions-menu"
+    :class="{ active: showMenu }"
+    @click="showMenu = !showMenu"
+  >
+    <slot name="display" />
+    <SmallModal
+      v-if="showMenu"
+      @close="showMenu = false"
+    >
+      <slot />
+    </SmallModal>
+  </div>
+</template>
+
+<script>
+import SmallModal from './SmallModal.vue';
+
+export default {
+  components: {
+    SmallModal,
+  },
+  data() {
+    return {
+      showMenu: false,
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.actions-menu {
+  position: relative;
+
+  &:hover {
+    cursor: pointer;
+  }
+
+  :deep(.content) {
+    padding: 8px;
+    border-radius: 12px;
+    background-color: rgb(44, 47, 54);
+    top: 50px;
+    right: 0;
+    white-space: nowrap;
+  }
+}
+</style>
---
+++ src/components/SmallModal.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="small-modal">
+    <div
+      class="overlay"
+      @click.stop="$emit('close')"
+    />
+    <div class="content">
+      <slot />
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.small-modal {
+  .overlay {
+    position: fixed;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    top: 0;
+  }
+
+  .content {
+    position: absolute;
+  }
+}
+</style>
