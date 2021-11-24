b40a7820d4a7d289ebbe4788e49884e7df995fc1
Nikita Cedrik <nikita.cedrik@gmail.com>
2021-11-24 15:45:30 +1000

    feat: add ImportPool view

---
+++ src/components/ButtonToken.vue
@@ -0,0 +1,72 @@
+<template>
+  <ButtonDefault
+    v-bind="$attrs"
+    class="button-token"
+    @click="showTokenModal"
+  >
+    <div class="token">
+      <div
+        v-if="$slots.icon"
+        class="icon"
+      >
+        <slot name="icon" />
+      </div>
+      <span>{{ title }}</span>
+    </div>
+    <img
+      v-if="arrow"
+      src="../assets/arrow.svg"
+    >
+  </ButtonDefault>
+</template>
+
+<script>
+import ButtonDefault from './ButtonDefault.vue';
+
+export default {
+  components: {
+    ButtonDefault,
+  },
+  props: {
+    title: { type: String, required: true },
+    arrow: { type: Boolean },
+  },
+  methods: {
+    async showTokenModal() {
+      this.$store.dispatch('modals/open', { name: 'select-token' });
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.button-token {
+  padding: 16px;
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  border-radius: 20px;
+
+  .token {
+    display: flex;
+    align-items: center;
+
+    .icon :deep(img) {
+      width: 24px;
+      height: 24px;
+      border-radius: 24px;
+    }
+
+    > span {
+      font-size: 18px;
+      margin-left: 12px;
+    }
+  }
+
+  img {
+    width: 24px;
+    height: 24px;
+  }
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import HomeView from '../views/HomeView.vue';
+import ImportPool from '../views/ImportPool.vue';
 
 const routes = [
   {
@@ -15,6 +16,12 @@ const routes = [
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue'),
   },
+  {
+    path: '/pool/import',
+    name: 'import-pool',
+    component: ImportPool,
+  },
+
 ];
 
 const router = createRouter({
