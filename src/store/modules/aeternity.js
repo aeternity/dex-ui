18974e6c60027558ca958db2d52a2ba931bd4949
Bogdan Manole <bogdan.manole@frago.ro>
2021-12-02 16:59:16 +0200

    feat: add slippage for router calls

--- src/store/modules/aeternity.js
+++ src/store/modules/aeternity.js
@@ -2,9 +2,6 @@ import routerInterface from '../../contracts/IAedexV2Router.aes';
 import waeInterface from '../../contracts/IWAE.aes';
 import aex9Inteface from '../../contracts/IAEX9Minimal.aes';
 
-// TODO: this should be reviewed
-const MaxUint256 = 115792089237316195423570985008687907853269984665640564039457584007913129639935n;
-
 const defaultDeadline = () => Date.now() + 30 * 60000;
 
 const getRouterInstance = (sdk) => sdk.getContractInstance(
@@ -23,10 +20,21 @@ const getAddress = (x) => x.deployInfo.address;
 const cttoak = (value) => value.replace('ct_', 'ak_');
 const getCtAddress = (contract) => cttoak(getAddress(contract));
 
-// TODO: is this the case?
+// TODO: remove this after testing the actual gas and before production
 const extraGas = {
   gas: 150000,
 };
+
+/**
+ * adds slippage to a given value
+ * NOTE: both values should be biging
+ * @async
+ * @param value given value
+ * @param slippage percentage (eg. 10,20...100)
+ * @return biging representing final value
+*/
+const addSlippage = (value, slippage) => value + (value * slippage) / 100;
+const subSlippage = (value, slippage) => value - (value * slippage) / 100;
 
 export default {
   namespaced: true,
@@ -34,6 +42,8 @@ export default {
   state: {
     router: null,
     wae: null,
+    // TODO: should this be the default?
+    slippage: 10n,
   },
 
   getters: {
@@ -45,6 +55,9 @@ export default {
     },
     setRouterInstance(state, instance) {
       state.router = instance;
+    },
+    setSlippage(state, slippage) {
+      state.slippage = slippage;
     },
   },
 
@@ -73,28 +86,28 @@ export default {
      * @param {string} p2.tokenA
      * @param {string} p2.tokenB
      * @param {bigint} p2.liquidity
-     * @param {bigint | null} p2.amountAMin
-     * @param {bigint | null} p2.amountBMin
+     * @param {bigint} p2.amountADesired
+     * @param {bigint} p2.amountBDesired
      * @param {bigint | null} p2.deadline
      * @return {Promise<[bigint,bigint]>}
      * amounts removed for tokenA and tokenB
     */
     async removeLiquidity({
-      state: { router },
+      state: { router, slippage },
       rootState: { address: to },
     }, {
       tokenA, tokenB,
       liquidity,
-      amountAMin,
-      amountBMin,
+      amountADesired,
+      amountBDesired,
       deadline,
     }) {
       const { decodedResult } = router.methods.remove_liquidity(
         tokenA,
         tokenB,
         liquidity,
-        amountAMin || 0n,
-        amountBMin || 0n,
+        subSlippage(amountADesired, slippage), // minumum amount to be removed
+        subSlippage(amountBDesired, slippage), // minumum amount to be removed
         to,
         deadline || defaultDeadline(),
         extraGas,
@@ -110,27 +123,27 @@ export default {
      * @param p1 vuex context
      * @param {string} p2.token
      * @param {bigint} p2.liquidity
-     * @param {bigint | null} p2.amountTokenMin
-     * @param {bigint | null} p2.amountAEMin
+     * @param {bigint} p2.amountTokenDesired
+     * @param {bigint} p2.amountAEDesired
      * @param {bigint | null} p2.deadline
      * @return {Promise<[bigint,bigint]>}
      * amounts removed for token and wae
     */
     async removeLiquidityAe({
-      state: { router },
+      state: { router, slippage },
       rootState: { address: to },
     }, {
       token,
       liquidity,
-      amountTokenMin,
-      amountAEMin,
+      amountTokenDesired,
+      amountAEDesired,
       deadline,
     }) {
       await router.methods.remove_liquidity_ae(
         token,
         liquidity,
-        amountTokenMin || 0n,
-        amountAEMin || 0n,
+        subSlippage(amountTokenDesired, slippage), // minumum amount to be removed
+        subSlippage(amountAEDesired, slippage), // minumum amount to be removed
         to,
         deadline || defaultDeadline(),
         extraGas,
@@ -146,8 +159,8 @@ export default {
      * @param {bigint} p2.amount
     */
     async createAllowance({
-      rootState: { sdk },
       state: { router },
+      rootState: { sdk },
     }, {
       token: tokenAddress,
       amount,
@@ -170,23 +183,17 @@ export default {
      * @param {string} p2.tokenB
      * @param {bigint} p2.amountADesired
      * @param {bigint} p2.amountBDesired
-     * @param {bigint | null} p2.amountAMin
-     * @param {bigint | null} p2.amountBMin
      * @param {bigint | null} p2.deadline
      * @return {Promise<[bigint,bigint,liquidity]>}
      * amounts transfered for tokenA and tokenB and the liquidity
     */
     async addLiquidity({
-      state: {
-        router,
-      },
+      state: { router, slippage },
       rootState: { address: to },
     }, {
       tokenA, tokenB,
       amountADesired,
       amountBDesired,
-      amountAMin,
-      amountBMin,
       deadline,
     }) {
       const { decodedResult } = await router.methods.add_liquidity(
@@ -194,8 +201,8 @@ export default {
         tokenB,
         amountADesired,
         amountBDesired,
-        amountAMin || 0n,
-        amountBMin || 0n,
+        subSlippage(amountADesired, slippage), // min amount to be added
+        subSlippage(amountBDesired, slippage), // min amount to be added
         to,
         deadline || defaultDeadline(),
         extraGas,
@@ -212,34 +219,28 @@ export default {
      * @param {string} p2.token
      * @param {bigint} p2.amountTokenDesired
      * @param {bigint} p2.amountAeDesired
-     * @param {bigint | null} p2.amountTokenMin
-     * @param {bigint | null} p2.amountAeMin
      * @param {bigint | null} p2.deadline
      * @return {Promise<[bigint,bigint,liquidity]>}
      * amounts transfered for token and AE and the liquidity
     */
     async addLiquidityAe({
-      state: {
-        router,
-      },
+      state: { router, slippage },
       rootState: { address: to },
     }, {
       token,
       amountTokenDesired,
       amountAeDesired,
-      amountTokenMin,
-      amountAeMin,
       deadline,
     }) {
       const { decodedResult } = await router.methods.add_liquidity_ae(
         token,
         amountTokenDesired,
-        amountTokenMin || 0n,
-        amountAeMin || 0n,
+        subSlippage(amountTokenDesired, slippage), // min amount
+        subSlippage(amountAeDesired, slippage), // min amount
         to,
         deadline || defaultDeadline(), {
           ...extraGas,
-          amount: amountAeDesired.toString(),
+          amount: amountAeDesired.toString(), // if less is added the diff is returned at the end
         },
       );
       return decodedResult;
@@ -251,20 +252,20 @@ export default {
      * allowing the router to transfer tokens from the first token balance
      * @param p1 vuex context
      * @param {bigint} p2.amountIn exact amount for the token found at path[0]
-     * @param {bigint | null} p2.amountOutMin minimal amount out for the token found at path[n-1]
+     * @param {bigint} p2.amountOutDesired desired amount out for the token found at path[n-1]
      * @returns {bigint[]} representing amounts out for every token from the path
     */
     async swapExactTokensForTokens({
-      state: { router },
+      state: { router, slippage },
       rootState: { address: to },
     }, {
       amountIn,
-      amountOutMin,
+      amountOutDesired,
       path,
     }) {
       const { decodedResult } = await router.methods.swap_exact_tokens_for_tokens(
         amountIn,
-        amountOutMin || 0n,
+        subSlippage(amountOutDesired, slippage),
         path,
         to,
         undefined,
@@ -279,21 +280,21 @@ export default {
      * allowing the router to transfer tokens from the first token balance
      * @param p1 vuex context
      * @param {bigint} p2.amountOut exact amount for the token found at path[n-1]
-     * @param {bigint | null} p2.amountInMax max amount in for the token found at path[0]
+     * @param {bigint} p2.amountInDesired desired amount in for the token found at path[0]
      * @returns {bigint[]} representing amounts in for every token from the path
     */
     async swapTokensForExactTokens({
-      state: { router },
+      state: { router, slippage },
       rootState: { address: to },
     }, {
       amountOut,
-      amountInMax,
+      amountInDesired,
       path,
       deadline,
     }) {
       const { decodedResult } = await router.methods.swap_tokens_for_exact_tokens(
         amountOut,
-        amountInMax ?? MaxUint256,
+        addSlippage(amountInDesired, slippage), // this is maximum
         path,
         to,
         deadline || defaultDeadline(),
@@ -306,20 +307,21 @@ export default {
     /**
      * @param p1 vuex context
      * @param {bigint} p2.amountAeIn exact amount in for the AE found at path[0]
-     * @param {bigint | null} p2.amountOutMin min amount out for the token found at path[n-1]
+     * @param {bigint} p2.amountOutDesired desired
+     * amount out for the token found at path[n-1]
      * @returns {bigint[]} representing amounts out for every token from the path
     */
     async swapExactAeForTokens({
-      state: { router },
+      state: { router, slippage },
       rootState: { address: to },
     }, {
       amountAeIn,
-      amountOutMin,
+      amountOutDesired,
       path,
       deadline,
     }) {
       const { decodedResult } = await router.methods.swap_exact_ae_for_tokens(
-        amountOutMin || 0n,
+        subSlippage(amountOutDesired, slippage),
         path,
         to,
         deadline || defaultDeadline(),
@@ -337,21 +339,21 @@ export default {
      * allowing the router to transfer tokens from the first token balance
      * @param p1 vuex context
      * @param {bigint} p2.amountAeOut exact amount out for the AE found at path[n-1]
-     * @param {bigint | null} p2.amountTokenInMax max amount in for the token found at path[0]
+     * @param {bigint} p2.amountTokenInDesired desired amount in for the token found at path[0]
      * @returns {bigint[]} representing amounts in for every token from the path
     */
     async swapTokensForExactAe({
-      state: { router },
+      state: { router, slippage },
       rootState: { address: to },
     }, {
       amountAeOut,
-      amountTokenInMax,
+      amountTokenInDesired,
       path,
       deadline,
     }) {
       const { decodedResult } = await router.methods.swap_tokens_for_exact_ae(
         amountAeOut,
-        amountTokenInMax ?? MaxUint256,
+        addSlippage(amountTokenInDesired, slippage), // not more than this
         path,
         to,
         deadline || defaultDeadline(),
@@ -367,21 +369,21 @@ export default {
      * allowing the router to transfer tokens from the first token balance
      * @param p1 vuex context
      * @param {bigint} p2.amountIn exact amount in for the token found at path[0]
-     * @param {bigint | null} p2.amountAeOutMin minimal amount out for the AE found at path[n-1]
+     * @param {bigint} p2.amountAeOutDesired desired amount out for the AE found at path[n-1]
      * @returns {bigint[]} representing amounts out for every token from the path
     */
     async swapExactTokensForAe({
-      state: { router },
+      state: { router, slippage },
       rootState: { address: to },
     }, {
       amountIn,
-      amountAeOutMin,
+      amountAeOutDesired,
       path,
       deadline,
     }) {
       const { decodedResult } = await router.methods.swap_exact_tokens_for_ae(
         amountIn,
-        amountAeOutMin ?? 0n,
+        subSlippage(amountAeOutDesired, slippage), // no less than this
         path,
         to,
         deadline || defaultDeadline(),
@@ -394,14 +396,14 @@ export default {
     /**
      * @param p1 vuex context
      * @param {bigint} p2.amountOut exact amount out for the token found at path[n-1]
-     * @param {bigint} p2.amountAeInMax max amount in for the AE found at path[0]
+     * @param {bigint} p2.amountAeInDesired desired amount in for the AE found at path[0]
      * @returns {bigint[]} representing amounts in for every token from the path
     */
     async swapAeForExactTokens({
-      state: { router },
+      state: { router, slippage },
       rootState: { address: to },
     }, {
-      amountAeInMax,
+      amountAeInDesired,
       amountOut,
       path,
       deadline,
@@ -414,7 +416,9 @@ export default {
         undefined,
         {
           ...extraGas,
-          amount: amountAeInMax.toString(),
+          // this is the diff between the desired+slippage and
+          // the actual amount will be return into owner's wallet
+          amount: addSlippage(amountAeInDesired, slippage).toString(),
         },
       );
       return decodedResult;
