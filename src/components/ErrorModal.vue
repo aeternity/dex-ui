13c5998ba2ca6932fd353c961ee025ec80cc6093
romart007 <labb0ssfreelance@gmail.com>
2021-12-09 02:38:20 +0800

    feat: add on adjustments for add error component

--- src/components/ErrorModal.vue
+++ src/components/ErrorModal.vue
@@ -1,18 +1,17 @@
 <template>
-  <ModalDefault class="error-modal" title="Error" close @close="resolve">
+  <ModalDefault
+    class="error-modal"
+    title="Error"
+    close
+    @close="resolve"
+  >
     <div class="error-details">
-        <img src="../assets/warning.svg" alt="Warning icon" />
-        <h4>{{ msg }}</h4>
+      <img src="../assets/alert.svg" alt="Warning icon" />
+      <h4>{{ msg }}</h4>
     </div>
-    <div>
-      <ButtonDefault
-        :class="['error-msg-btn']"
-        fill="transparent-blue"
-        @click.prevent="resolve"
-      >
-        <span class="name">Dismiss</span>
-      </ButtonDefault>
-    </div>
+    <ButtonDefault fill="transparent-blue" @click="resolve">
+      <span>Dismiss</span>
+    </ButtonDefault>
   </ModalDefault>
 </template>
 
@@ -31,35 +30,29 @@ export default {
     resolve: { type: Function, required: true },
     close: { type: Function, default: null },
   },
-  data() {
-    return {
-      showMenu: false,
-    };
-  },
 };
 </script>
 
 <style lang="scss" scoped>
 .error-modal {
-    .error-details {
-        padding: 20px 30px;
-        max-width: 480px;
+  .error-details {
+    padding: 20px 30px;
+    max-width: 480px;
 
-        img {
-            max-width: 50px;
-        }
-
-        h4 {
-          color: white;
-          margin-top: 5px;
-        }
+    img {
+      max-width: 50px;
     }
 
-    :deep(.error-msg-btn) {
-        padding: 8px 12px;
-        margin-bottom: 20px;
-        white-space: nowrap;
-        font-size: 18px;
+    h4 {
+      color: white;
+      margin-top: 5px;
     }
+  }
+
+  .button-default {
+    padding: 8px 12px;
+    margin-bottom: 20px;
+    font-size: 18px;
+  }
 }
 </style>
