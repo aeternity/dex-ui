4232f05c5465582ec0caaadd479b320cb02d8a11
Bogdan Manole <bogdan.manole@frago.ro>
2021-12-28 13:07:46 +0200

    chore(lint): adding space infix-operator rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,6 +14,7 @@ module.exports = {
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'space-infix-ops': ['error', { int32Hint: false }],
   },
   overrides: [
     {
