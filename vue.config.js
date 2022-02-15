8cdb85f34fbad5205bfc5dfd660f5742128604a3
badi <badi1wow@gmail.com>
2022-02-15 21:15:36 +0300

    feat: added inline svg loader #46

--- vue.config.js
+++ vue.config.js
@@ -9,6 +9,27 @@ module.exports = defineConfig({
       .use('raw-loader')
       .loader('raw-loader')
       .end();
+
+    const svgRule = config.module.rule('svg');
+    svgRule.uses.clear();
+    svgRule
+      .delete('type')
+      .delete('generator')
+      .oneOf('vue-component')
+      .resourceQuery(/vue-component/)
+      .use('vue-loader')
+      .loader('vue-loader')
+      .end()
+      .use('vue-svg-loader')
+      .loader('vue-svg-loader')
+      .end()
+      .end()
+      .oneOf('external')
+      .use('file-loader')
+      .loader('file-loader')
+      .options({
+        name: 'assets/[name].[hash:8].[ext]',
+      });
   },
   configureWebpack: {
     plugins: [
