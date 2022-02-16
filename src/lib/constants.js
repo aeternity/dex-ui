dec63416884193cda30de86e22c1306839bbfbb4
Nikita Cedrik <nikita.cedrik@gmail.com>
2022-02-16 14:03:55 +1000

    feat: add ability to set deadline

--- src/lib/constants.js
+++ src/lib/constants.js
@@ -5,3 +5,7 @@ export const MINIMUM_LIQUIDITY = 1000n;
 export const DEFAULT_SLIPPAGE = 5;
 export const MIN_SLIPPAGE = 5;
 export const MAX_SLIPPAGE = 51;
+
+export const DEFAULT_DEADLINE = 30;
+export const MIN_DEADLINE = 1;
+export const MAX_DEADLINE = 1000;
