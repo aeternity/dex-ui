f2569aa15598a51e7365012a295feb156f7dffa7
badi <badi1wow@gmail.com>
2022-03-07 14:36:53 +0300

    feat: added sentry #202

--- src/main.js
+++ src/main.js
@@ -1,5 +1,8 @@
 import { createApp } from 'vue';
 import { sync } from 'vuex-router-sync';
+import * as Sentry from '@sentry/vue';
+import { BrowserTracing } from '@sentry/tracing';
+
 import App from './App.vue';
 import router from './router';
 import store from './store';
@@ -25,5 +28,20 @@ app.config.globalProperties.$isMobile = window.navigator.userAgent.includes('Mob
 registerModals();
 
 sync(store, router);
+
+Sentry.init({
+  app,
+  dsn: process.env.VUE_APP_SENTRY_URL,
+  integrations: [
+    new BrowserTracing({
+      routingInstrumentation: Sentry.vueRouterInstrumentation(router),
+      tracingOrigins: ['dex.prd.aepps.com', 'dex.dev.aepps.com', 'dex.aepps.com', /^\//],
+    }),
+  ],
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  // We recommend adjusting this value in production
+  tracesSampleRate: 1.0,
+});
 
 app.use(store).use(router).mount('#app');
