b2a19b7a2772dc4d95281c593a2507410b83b2d6
CedrikNikita <nikita.cedrik@gmail.com>
2022-04-07 17:33:36 +1000

    feat: notify user if connection is lost

---
+++ src/components/NotificationConnectionStatus.vue
@@ -0,0 +1,35 @@
+<template>
+  <NotificationDefault
+    v-bind="$attrs"
+    close
+    class="notification-connection-status"
+  >
+    <span>{{ text }}</span>
+  </NotificationDefault>
+</template>
+
+<script>
+import NotificationDefault from './NotificationDefault.vue';
+
+export default {
+  components: { NotificationDefault },
+  props: {
+    text: { type: String, required: true },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+@use '../styles/variables.scss';
+
+.notification-connection-status {
+  position: fixed;
+  bottom: 35%;
+  right: 1%;
+  background-color: variables.$color-orange;
+
+  :deep(.content) {
+    text-align: left;
+  }
+}
+</style>
--- src/store/plugins/connectionStatusTracker.js
+++ src/store/plugins/connectionStatusTracker.js
@@ -1,6 +1,12 @@
 export default (store) => {
   const connectionStatusHandler = () => {
     if (store.state.onLine !== navigator.onLine) store.commit('setOnLine', navigator.onLine);
+    if (!navigator.onLine && !window.navigator.userAgent.includes('Mobi')) {
+      store.dispatch('modals/open', {
+        name: 'connection-status',
+        text: 'You are offline... Please check your connection.',
+      });
+    }
   };
 
   window.addEventListener('online', connectionStatusHandler);
