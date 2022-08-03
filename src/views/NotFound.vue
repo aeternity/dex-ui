5a3528f3af6e7e2c55e082f2a2323a4f121a6274
Keno Dressel <hire@keno.digital>
2022-08-03 17:41:49 +0200

    feat: adds 404 fallback page

---
+++ src/views/NotFound.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="not-found">
+    <h1>404. Sorry.</h1>
+  </div>
+</template>
+<style>
+.not-found {
+  display: flex;
+  color: white;
+  padding-top: 20px;
+  justify-content: center;
+  font-size: 3rem;
+}
+</style>
