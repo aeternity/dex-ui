7f75159b9115ed21e37fbaf029b2266cd4b330ca
Keno Dressel <hire@keno.digital>
2022-08-08 09:53:40 +0200

    feat: adds 2 aex9 tokens

--- src/lib/constants.js
+++ src/lib/constants.js
@@ -37,6 +37,18 @@ export const DEFAULT_NETWORKS = [
         symbol: 'WAE',
         is_ae: false,
       },
+      {
+        contract_id: 'ct_KeTvHnhU85vuuQMMZocaiYkPL9tkoavDRT3Jsy47LK2YqLHYb',
+        decimals: MAGNITUDE,
+        name: 'WeTrue Token',
+        symbol: 'WTT',
+      },
+      {
+        contract_id: 'ct_7UfopTwsRuLGFEcsScbYgQ6YnySXuyMxQWhw6fjycnzS5Nyzq',
+        decimals: MAGNITUDE,
+        name: 'AeBox Coin',
+        symbol: 'ABC',
+      },
     ],
   },
   {
