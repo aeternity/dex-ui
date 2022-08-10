4e9eb0d421080cbe0ef373382219afa120ec0575
CedrikNikita <nikita.cedrik@gmail.com>
2022-08-10 18:17:13 +1000

    fix(svg): make skip optimize flag works correctly

--- vue-svg-loader.js
+++ vue-svg-loader.js
@@ -1,7 +1,7 @@
 const { optimize } = require('svgo');
 
 module.exports = function vueSvgLoader(svg) {
-  const { svgo: svgoConfig } = this.getOptions || {};
+  const { svgo: svgoConfig } = this.getOptions() || {};
 
   if (svgoConfig !== false) {
     // eslint-disable-next-line no-param-reassign
