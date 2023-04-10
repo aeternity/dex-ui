a5eacfcedc6c3133b65b828f731f650ec3fc7fd7
Bogdan Manole <bogdan.manole@frago.ro>
2023-04-10 16:53:20 +0300

    feat: add extra info for low slippage errors

---
+++ src/lib/errors.js
@@ -0,0 +1,9 @@
+const lowSlippageMessage = 'slippage is too low';
+module.exports = {
+  'AedexV2Library: INSUFFICIENT_OUTPUT_AMOUNT': lowSlippageMessage,
+  'AedexV2Router: INSUFFICIENT_OUTPUT_AMOUNT': lowSlippageMessage,
+  'AedexV2Pair: INSUFFICIENT_OUTPUT_AMOUNT': lowSlippageMessage,
+  'AedexV2Library: INSUFFICIENT_INPUT_AMOUNT': lowSlippageMessage,
+  'AedexV2Pair: INSUFFICIENT_INPUT_AMOUNT': lowSlippageMessage,
+  'AedexV2Router: EXCESSIVE_INPUT_AMOUNT': lowSlippageMessage,
+};
