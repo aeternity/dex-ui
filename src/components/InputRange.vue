25ade865244263ec1cf36fca69296d9c1bfaef8b
Timo Erdelt <timo@unit214.de>
2024-02-16 13:31:03 +0100

    test(deps): migrate to vitest and upgrade cypress

--- src/components/InputRange.vue
+++ src/components/InputRange.vue
@@ -21,7 +21,7 @@ export default {
 <style lang="scss" scoped>
 @use '../styles/variables.scss';
 
-input[type=range] {
+input[type='range'] {
   -webkit-appearance: none;
   width: 100%;
   background: transparent;
