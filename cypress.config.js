bda7fb50e7f5311bb3187cd3d197f70c9a05c23e
Timo Erdelt <timo@unit214.de>
2024-02-21 15:13:51 +0100

    ci: fix npm run test:e2e

--- cypress.config.js
+++ cypress.config.js
@@ -3,7 +3,7 @@ import { defineConfig } from 'cypress';
 export default defineConfig({
   e2e: {
     specPattern: 'tests/e2e/**/*.{cy,spec}.{js,jsx,ts,tsx}',
-    baseUrl: 'http://localhost:4173',
+    baseUrl: 'http://localhost:5173',
     supportFile: 'tests/e2e/support/index.js',
     chromeWebSecurity: false,
     screenshotOnRunFailure: false,
