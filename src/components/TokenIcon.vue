aa65c52f42a8faeba3ce43f79c80929e1aa12e45
Keno Dressel <hire@keno.digital>
2024-05-08 16:48:06 +0200

    fix: properly display the icon

--- src/components/TokenIcon.vue
+++ src/components/TokenIcon.vue
@@ -1,12 +1,16 @@
 <template>
   <div class="token-icon">
     <img
-      v-if="tokenA"
+      v-if="tokenA && !tokenA.is_ae"
       :class="{ rotating, 'not-pool-token': !tokenB }"
       :src="mapToken(tokenA)"
       alt=""
     />
-    <img v-if="tokenB" :class="{ rotating }" :src="mapToken(tokenB)" alt="" />
+    <AeIcon
+        v-else-if="tokenA && tokenA.is_ae"
+        :class="{ rotating, 'not-pool-token': !tokenB }" />
+    <img v-if="tokenB && !tokenB.is_ae" :class="{ rotating }" :src="mapToken(tokenB)" alt="" />
+    <AeIcon v-else-if="tokenB && tokenB.is_ae" />
   </div>
 </template>
 
@@ -14,18 +18,19 @@
 import AeIcon from '@/assets/ae.svg';
 
 export default {
+  components: {
+    AeIcon,
+  },
   props: {
-    tokenA: { type: Object, required: true },
+    tokenA: { type: Object, default: null },
     tokenB: { type: Object, default: null },
     rotating: { type: Boolean },
   },
   methods: {
     mapToken(token) {
-      return token.is_ae !== undefined
-        ? AeIcon
-        : `https://avatars.z52da5wt.xyz/${token.contract_id}`;
+        return `https://avatars.z52da5wt.xyz/${token.contract_id}`;
     },
-  },
+  }
 };
 </script>
 
@@ -33,13 +38,13 @@ export default {
 .token-icon {
   display: flex;
 
-  img {
+  img, svg {
     height: 24px;
     width: 24px;
     border-radius: 24px;
   }
 
-  img:not(.not-pool-token):nth-of-type(2) {
+  img:not(.not-pool-token):nth-of-type(2), svg:not(.not-pool-token):nth-of-type(2) {
     margin-left: -10px;
   }
 }
