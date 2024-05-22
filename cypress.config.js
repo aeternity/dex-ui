f3c3ba31cc5ea3b6bc09776784c954058b0db13f
Keno Dressel <keno@unit214.de>
2024-05-22 08:55:01 +0200

    test(e2e): updates cypress config and adds user agent

--- cypress.config.js
+++ cypress.config.js
@@ -5,8 +5,12 @@ export default defineConfig({
     specPattern: 'tests/e2e/**/*.{cy,spec}.{js,jsx,ts,tsx}',
     baseUrl: 'http://localhost:5173',
     supportFile: 'tests/e2e/support/index.js',
-    chromeWebSecurity: false,
-    screenshotOnRunFailure: false,
-    video: false,
   },
+  chromeWebSecurity: false,
+  video: false,
+  screenshotOnRunFailure: false,
+  userAgent:
+    'Mozilla/5.0 (iPhone; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1',
+  viewportHeight: 667,
+  viewportWidth: 375,
 });
