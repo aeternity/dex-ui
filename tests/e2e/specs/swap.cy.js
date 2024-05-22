86ebc1881fae6c32ac06014be15acd519f885825
Keno Dressel <keno@unit214.de>
2024-05-22 08:55:01 +0200

    test(e2e): updates cypress config and adds user agent

--- tests/e2e/specs/swap.cy.js
+++ tests/e2e/specs/swap.cy.js
@@ -1,7 +1,7 @@
 // https://docs.cypress.io/api/introduction/api.html
 
 describe('My First Test', () => {
-  it.skip('Visits the app root url', () => {
+  it('Visits the app root url', () => {
     cy.visit('/')
       .contains('.title', 'Swap')
       .login()
