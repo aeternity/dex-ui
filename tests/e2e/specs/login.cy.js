2bf9af41a6ea2696552c985bc11f5fbb523a24fb
Keno Dressel <keno@unit214.de>
2024-05-23 11:06:59 +0200

    test(e2e): adds tests for initial login

---
+++ tests/e2e/specs/login.cy.js
@@ -0,0 +1,34 @@
+describe('Login', () => {
+  it('Calls the wallet for login', () => {
+    cy.visit('/')
+      .contains('.title', 'Swap')
+      .get('[data-cy=connect-wallet]')
+      .click()
+      .get('[data-cy=connect-Superhero]', { timeout: 60000 })
+      .click()
+      .url()
+      .should('eql', 'https://wallet.superhero.com/');
+  });
+
+  it('Receive the wallet callback testnet', () => {
+    cy.login();
+  });
+
+  it('Receive the wallet callback mainnet', () => {
+    cy.visit(
+      '/swap?address=ak_rRVV9aDnmmLriPePDSvfTUvepZtR2rbYk2Mx4GCqGLcc1DMAq&networkId=ae_mainnet',
+    )
+      .contains('.title', 'Swap')
+      // popup should open with "Thanks for trying out the DEX!"
+      .get('.about-dex-modal')
+      .should('be.visible')
+      // close the popup
+      .get('.close')
+      .click()
+      .get('.about-dex-modal')
+      .should('not.exist')
+      // check that mainnet is written in the network selector
+      .get('.active-network')
+      .should('contain', 'Mainnet');
+  });
+});
