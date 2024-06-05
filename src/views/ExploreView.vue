9d4db0b8f6a750b9604c521a5a063efc567cab3c
Keno Dressel <keno@unit214.de>
2024-06-05 14:07:59 +0200

    feat: first steps towards explore

---
+++ src/views/ExploreView.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="explore-wrapper">
+    <StyledGraph> </StyledGraph>
+  </div>
+</template>
+<script>
+import { defineComponent } from 'vue';
+import StyledGraph from '@/components/explore/StyledGraph.vue';
+
+export default defineComponent({
+  components: { StyledGraph },
+});
+</script>
+<style lang="scss" scoped>
+@use '../styles/variables.scss';
+@use '../styles/typography.scss';
+@use '../styles/mixins.scss';
+
+.explore-wrapper {
+  display: flex;
+  flex-direction: column;
+  margin: 4vh auto 0;
+  border-radius: 24px;
+  max-width: 1280px;
+  background: variables.$color-black3;
+  box-shadow:
+    rgb(0 0 0 / 1%) 0 0 1px,
+    rgb(0 0 0 / 4%) 0 4px 8px,
+    rgb(0 0 0 / 4%) 0 16px 24px,
+    rgb(0 0 0 / 1%) 0 24px 32px;
+}
+</style>
---
+++ src/views/TokenDetailView.vue
@@ -0,0 +1,25 @@
+<template>
+  <ExploreWrapper>
+    <div>BREADCRUMB</div>
+    <div>
+      <span>LOGO</span>
+      AE / Aeternity
+    </div>
+    <!-- Graph that shows price over time -->
+    <!-- Stats -->
+    <!-- List of transactions -->
+    <!-- List of pools -->
+  </ExploreWrapper>
+</template>
+<script>
+import { defineComponent } from 'vue';
+import ExploreWrapper from '@/components/explore/ExploreWrapper.vue';
+
+export default defineComponent({
+  components: { ExploreWrapper },
+  mounted() {
+    // Fetch data
+  },
+});
+</script>
+<style lang="scss" scoped></style>
