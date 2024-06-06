78e14c4750a7d604c663705f2af938649c965394
Keno Dressel <hire@keno.digital>
2024-06-06 11:49:50 +0200

    refactor: moves avatar to component

---
+++ src/components/AddressAvatar.vue
@@ -0,0 +1,15 @@
+<template>
+  <img :src="`https://avatars.z52da5wt.xyz/${address}`" :alt="address" />
+</template>
+<script>
+export default {
+  name: 'AddressAvatar',
+  props: {
+    address: {
+      type: String,
+      required: true,
+    },
+  },
+};
+</script>
+<style scoped lang="scss"></style>
