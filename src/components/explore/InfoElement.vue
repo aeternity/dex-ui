d00877cf87bee6d03e190fd2f37a51f572112764
Keno Dressel <hire@keno.digital>
2024-06-17 15:44:00 +0200

    feat(pool): adds and abstracts info element

---
+++ src/components/explore/InfoElement.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    <div class="mb-1 text-gray-400">{{ title }}</div>
+    <div v-if="link">
+      <a class="flex gap-2" :href="link" target="_blank" rel="noopener noreferrer">
+        {{ value }}
+        <ExternalLinkIcon aria-hidden="true" />
+        <span class="sr-only">External link</span>
+      </a>
+    </div>
+    <div v-else>
+      {{ value }}
+    </div>
+  </div>
+</template>
+<script>
+import ExternalLinkIcon from '@/assets/external-link.svg';
+
+export default {
+  name: 'InfoElement',
+  components: { ExternalLinkIcon },
+  props: {
+    value: {
+      type: String,
+      required: true,
+    },
+    link: {
+      type: String,
+      required: false,
+      default: null,
+    },
+    title: {
+      type: String,
+      required: true,
+    },
+  },
+};
+</script>
+<style scoped lang="scss">
+svg {
+  width: 18px;
+  height: 18px;
+  margin-top: 3px;
+}
+</style>
---
+++ src/components/explore/PoolTable.vue
@@ -0,0 +1,8 @@
+<template></template>
+
+<script>
+export default {
+  name: 'PoolTable',
+};
+</script>
+<style scoped lang="scss"></style>
