be4c7d30351dfd1826f837c4e1ac0979ad977dc4
Keno Dressel <hire@keno.digital>
2024-06-20 12:33:54 +0200

    refactor(graph): moves data manipulation to computed property

--- src/components/explore/PriceHistoryGraph.vue
+++ src/components/explore/PriceHistoryGraph.vue
@@ -156,22 +156,63 @@ export default {
     },
     filteredData() {
       const selectedDataSet = this.datasets.find((d) => d.label === this.selectedChart);
-      return {
+      const minTime =
+        this.selectedTimeFrame === 'MAX'
+          ? Math.min(...this.x)
+          : Date.now() - 1000 * 60 * 60 * this.timeFrames[this.selectedTimeFrame];
+
+      const data = {
         filteredData: selectedDataSet.data
-          .filter(
-            (_, i) =>
-              this.x[i] > Date.now() - 1000 * 60 * 60 * this.timeFrames[this.selectedTimeFrame],
-          )
+          .filter((_, i) => this.x[i] >= minTime)
+          .filter((d) => !new BigNumber(d).isNaN()),
+        excludedData: selectedDataSet.data
+          .filter((_, i) => this.x[i] < minTime)
           .filter((d) => !new BigNumber(d).isNaN()),
         filteredTime: this.x
-          .filter((d) => d > Date.now() - 1000 * 60 * 60 * this.timeFrames[this.selectedTimeFrame])
+          .filter((_, i) => !new BigNumber(selectedDataSet.data[i]).isNaN())
+          .filter((d) => d >= minTime)
           .map((d) => Number(d)),
+        excludedTime: this.x
+          .filter((_, i) => !new BigNumber(selectedDataSet.data[i]).isNaN())
+          .filter((d) => d < minTime)
+          .map((d) => Number(d)),
+      };
+
+      // interpolate data to show full frame
+      if (
+        (['TVL', 'Locked'].includes(this.selectedChart) || this.selectedChart.includes('Price')) &&
+        data.excludedData.length > 0
+      ) {
+        // all of these are aggregated and summed, so we need to have baseline
+        data.filteredData.unshift(data.excludedData.pop());
+        data.filteredTime.unshift(minTime);
+        data.excludedTime.pop();
+      }
+
+      if (['Volume', 'Fees'].includes(this.selectedChart) && data.filteredData.length > 0) {
+        // these just show the last value, so we need to have a baseline for the graph time but no value
+        // if there is no data, we do not need to add anything as we can show "no data"
+        data.filteredData.unshift(0);
+        data.filteredTime.unshift(minTime);
+      }
+
+      if (this.selectedChart.includes('Price')) {
+        // as these always have current value, we need to add it to the end
+        // theoretically this is also required for TVL and locked, but we interpolate those in the graph based
+        // on the last value, so we don't need to add it here
+        data.filteredData.push(data.filteredData[data.filteredData.length - 1]);
+        data.filteredTime.push(Date.now());
+      }
+
+      return {
+        ...data,
         selectedDataSet,
+        minTime,
       };
     },
     graphData() {
       // filter data based on selected time
-      const { filteredTime, filteredData, selectedDataSet } = this.filteredData;
+      const { filteredTime, filteredData, selectedDataSet, minTime } = this.filteredData;
       if (
         (filteredData.length === 0 || filteredTime.length === 0 || !selectedDataSet) &&
         (this.selectedChart === 'Fees' || this.selectedChart === 'Volume')
@@ -184,11 +225,6 @@ export default {
 
       // aggregate data based on selected time
       // retrieve min time from data or default to selected view
-      const minTime =
-        filteredTime.length > 0
-          ? Math.min(...filteredTime)
-          : Date.now() - 1000 * 60 * 60 * this.timeFrames[this.selectedTimeFrame];
-
       if (['TVL', 'Volume', 'Fees', 'Locked'].includes(this.selectedChart)) {
         // these three charts are bar charts, so we need to calculate buckets
         const bucketSize = (Date.now() - minTime) / 30;
@@ -200,12 +236,6 @@ export default {
             return [key, []];
           }),
         );
-
-        // deal with no data in current time frame
-        if (Object.keys(filteredData).length === 0 && selectedDataSet.data.length > 0) {
-          filteredTime.push(minTime);
-          filteredData.push(selectedDataSet.data[selectedDataSet.data.length - 1]);
-        }
 
         const aggregatedData = filteredData.reduce((acc, d, i) => {
           const time = filteredTime[i];
@@ -233,8 +263,6 @@ export default {
               return [time, aggregatedValue];
             }),
           );
-
-          // deal with empty filteredData
         } else {
           // sum fees and volume
           bucketedData = Object.fromEntries(
@@ -256,18 +284,13 @@ export default {
           ],
         };
       }
-      // Prices should be interpolated
-      if (filteredData.length === 0 && selectedDataSet.data.length > 0) {
-        filteredTime.push(minTime);
-        filteredData.push(selectedDataSet.data[selectedDataSet.data.length - 1]);
-      }
 
       return {
-        labels: [...filteredTime.map((x) => Number(x)), Date.now()],
+        labels: filteredTime.map((x) => Number(x)),
         datasets: [
           {
             label: selectedDataSet.label,
-            data: [...filteredData, filteredData[filteredData.length - 1]].map((y) => Number(y)),
+            data: filteredData.map((y) => Number(y)),
             borderColor: 'rgb(0 255 157 / 80%)',
             backgroundColor: 'rgb(0 255 157 / 80%)',
           },
