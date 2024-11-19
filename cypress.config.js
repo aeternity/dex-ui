97fee406f536649910cd06fe9974fd90f317fe77
Timo Erdelt <timo@unit214.de>
2024-11-19 17:08:26 +0100

    fix: increase timeout of e2e tests

--- cypress.config.js
+++ cypress.config.js
@@ -13,4 +13,5 @@ export default defineConfig({
     'Mozilla/5.0 (iPhone; CPU iPhone OS 17_5_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.4.1 Mobile/15E148 Safari/604.1',
   viewportHeight: 800,
   viewportWidth: 400,
+  defaultCommandTimeout: 20000,
 });
