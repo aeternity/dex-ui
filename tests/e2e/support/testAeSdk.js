d4cb2961ac5b8a440a8c3b6e491faf203796522f
Nikita <nikita.cedrik@gmail.com>
2026-09-11 17:22:57 +1000

    feat: update sdk to 15.0.0

--- tests/e2e/support/testAeSdk.js
+++ tests/e2e/support/testAeSdk.js
@@ -1,7 +1,8 @@
 import { AeSdk, Node, MemoryAccount } from '@aeternity/aepp-sdk';
 
-const PAYER_ACCOUNT_SECRET_KEY =
-  '9ebd7beda0c79af72a42ece3821a56eff16359b6df376cf049aee995565f022f840c974b97164776454ba119d84edc4d6058a8dec92b6edc578ab2d30b4c4200';
+// sk_-prefixed secret key (aepp-sdk@14+ format), same account as the previous hex key
+// ak_21A27UVVt3hDkBE5J7rhhqnH5YNb4Y1dqo4PnSybrH85pnWo7E
+const PAYER_ACCOUNT_SECRET_KEY = 'sk_2CuofqWZHrABCrM7GY95YSQn8PyFvKQadnvFnpwhjUnDCFAWmf';
 const NODE_URL = 'https://testnet.aeternity.io';
 
 const payerAccount = new MemoryAccount(PAYER_ACCOUNT_SECRET_KEY);
